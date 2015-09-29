6249eed8a9a78fe8c325ee31d98290c763a4137e
Karol Marcjan <karol.marcjan@gmail.com>
2015-09-30 00:59:13 +0200

    Move planet-related code out into a separate module

--- ThreeBodies.elm
+++ ThreeBodies.elm
@@ -1,9 +1,11 @@
 module ThreeBodies where
 
+import Planet
+
 import Html exposing (..)
 import Html.Events exposing (..)
 
-import Color exposing(red, black)
+import Color exposing (red, black)
 
 import Graphics.Element as G
 import Graphics.Collage as C
@@ -14,43 +16,10 @@ main =
   div []
       [ h1 [] [ text "The three body problem" ]
       , p  [] [ text problemDescription ]
-      , view model
+      , Planet.view planets
       ]
 
-type alias Planet = { x : Float, y : Float, mass : Float, radius : Float }
-
-view model =
-  let
-    (width, height) = sizeOfCanvas 40 model
-  in
-    List.map viewPlanet model
-      |> C.collage width height
-      |> G.color black
-      |> fromElement
-
-sizeOfCanvas margin planets =
-  let
-    absMaxOr default selector = planets
-      |> List.map selector
-      |> List.map abs
-      |> List.maximum
-      |> Maybe.withDefault default
-
-    maxRadius = absMaxOr 0.0 .radius
-
-    ensureFit selector = absMaxOr 0.0 selector
-      |> \maxAbsValue -> 2.0 * (maxAbsValue + maxRadius)
-      |> ceiling
-      |> (\maxPos -> maxPos + margin)
-  in
-    (ensureFit .x, ensureFit .y)
-
-viewPlanet planet =
-  C.circle planet.radius
-    |> C.filled red
-    |> C.move (planet.x, planet.y)
-
-model =
+planets =
   [ { x =   0.0, y = -20.0, mass = 10.0, radius = 30.0}
   , { x = 100.0, y =   0.0, mass = 10.0, radius = 30.0}
   , { x = -70.0, y =  60.0, mass = 10.0, radius = 30.0}
