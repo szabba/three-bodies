5c97c69d82c92e3982102cad728e2a29f9f59e7c
Karol Marcjan <karol.marcjan@gmail.com>
2015-09-30 00:48:25 +0200

    Refactor planet displaying code

--- ThreeBodies.elm
+++ ThreeBodies.elm
@@ -3,7 +3,7 @@ module ThreeBodies where
 import Html exposing (..)
 import Html.Events exposing (..)
 
-import Color exposing(red)
+import Color exposing(red, black)
 
 import Graphics.Element as G
 import Graphics.Collage as C
@@ -17,16 +17,43 @@ main =
       , view model
       ]
 
+type alias Planet = { x : Float, y : Float, mass : Float, radius : Float }
+
 view model =
   let
-    planet = C.filled red (C.circle 30)
-    combined = C.collage 100 100 [planet]
-  in fromElement combined
+    (width, height) = sizeOfCanvas 40 model
+  in
+    List.map viewPlanet model
+      |> C.collage width height
+      |> G.color black
+      |> fromElement
+
+sizeOfCanvas margin planets =
+  let
+    absMaxOr default selector = planets
+      |> List.map selector
+      |> List.map abs
+      |> List.maximum
+      |> Maybe.withDefault default
+
+    maxRadius = absMaxOr 0.0 .radius
+
+    ensureFit selector = absMaxOr 0.0 selector
+      |> \maxAbsValue -> 2.0 * (maxAbsValue + maxRadius)
+      |> ceiling
+      |> (\maxPos -> maxPos + margin)
+  in
+    (ensureFit .x, ensureFit .y)
+
+viewPlanet planet =
+  C.circle planet.radius
+    |> C.filled red
+    |> C.move (planet.x, planet.y)
 
 model =
-  [ { x = 0,   y = 0,   mass = 10, radius = 30}
-  , { x = 100, y = 0,   mass = 10, radius = 30}
-  , { x = 0,   y = 200, mass = 10, radius = 30}
+  [ { x =   0.0, y = -20.0, mass = 10.0, radius = 30.0}
+  , { x = 100.0, y =   0.0, mass = 10.0, radius = 30.0}
+  , { x = -70.0, y =  60.0, mass = 10.0, radius = 30.0}
   ]
 
 problemDescription =
