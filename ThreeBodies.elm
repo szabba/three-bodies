2c80f04a46d2eff017a29a0be3d85d1dcfccf2ea
Karol Marcjan <karol.marcjan@gmail.com>
2015-09-30 00:10:06 +0200

    Get something to display

--- ThreeBodies.elm
+++ ThreeBodies.elm
@@ -3,13 +3,31 @@ module ThreeBodies where
 import Html exposing (..)
 import Html.Events exposing (..)
 
+import Color exposing(red)
+
+import Graphics.Element as G
+import Graphics.Collage as C
+
 import String
 
 main =
   div []
       [ h1 [] [ text "The three body problem" ]
       , p  [] [ text problemDescription ]
+      , view model
       ]
+
+view model =
+  let
+    planet = C.filled red (C.circle 30)
+    combined = C.collage 100 100 [planet]
+  in fromElement combined
+
+model =
+  [ { x = 0,   y = 0,   mass = 10, radius = 30}
+  , { x = 100, y = 0,   mass = 10, radius = 30}
+  , { x = 0,   y = 200, mass = 10, radius = 30}
+  ]
 
 problemDescription =
   String.concat
